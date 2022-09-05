c8058b25c112400399edd5d2c155befcff463a44
GitHub Action <action@github.com>
2022-09-05 07:27:52 +0000

    Template cleanup

--- src/main/kotlin/com/github/jerrybelaston/toto/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/jerrybelaston/toto/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.jerrybelaston.toto.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.jerrybelaston.toto.MyBundle
 
 class MyApplicationService {
 
--- src/main/kotlin/com/github/jerrybelaston/toto/services/MyProjectService.kt
+++ src/main/kotlin/com/github/jerrybelaston/toto/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.jerrybelaston.toto.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.jerrybelaston.toto.MyBundle
 
 class MyProjectService(project: Project) {
 
